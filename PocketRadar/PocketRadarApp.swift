f44d0d9cd841e5f99a68eea140483cf3b132903e
Brandon <brndnwood@gmail.com>
2022-06-02 16:18:25 -0500

    Initial Commit

---
+++ PocketRadar/PocketRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketRadarApp.swift
+//  PocketRadar
+//
+//  Created by Brandon Wood on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PocketRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
